1e4d60a7ac759c295de603883d758ff3300d7a09
KateJG <vbs.jgs@gmail.com>
2018-04-23 09:49:44 +0100

    Shorter way

--- 1.JavaSyntax/src/com/javarush/task/task06/task0622/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0622/Solution.java
@@ -12,38 +12,23 @@ public class Solution {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         //напишите тут ваш код
-
         int[] arr = new int[5];
 
-        for (int i = 0; i < arr.length; i++)
-        {
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = Integer.parseInt(reader.readLine());
         }
 
-        int [] bubble = bubbleSort(arr);
-        for(int i = 0; i < arr.length; i ++)
-            System.out.println(bubble[i]);
-    }
+        for (int i = 0; i < arr.length; i++) {
 
-    public static int[] bubbleSort(int[] arr)
-    {
-        for(int i = arr.length - 1; i > 0; i--)
-        {
-            for(int j = 0; j < i; j++)
-            {
-                if(arr[j] > arr[j + 1]){
+            for (int j = 0; j < arr.length - 1; j++) {
+
+                if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
                 }
             }
+            System.out.println(arr[i]);
         }
-        return arr;
     }
 }
-
-
-
-
-
-
