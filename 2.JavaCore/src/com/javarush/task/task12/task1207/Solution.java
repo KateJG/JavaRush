ba5945abc8371f3389366ee5b40bed73646317d4
KateJG <vbs.jgs@gmail.com>
2018-07-02 09:32:38 +0100

    Upload

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+        print(8);
+        //Integer x = new Integer(100);
+        //print(new Integer(50)); // съедает больше памяти. ****eats more memory*****
+
+        print((Integer) 100);
+
+    }
+
+    //Напишите тут ваши методы
+    static void print (int n) {
+
+    }
+
+    static void print (Integer m) {
+
+    }
+
+
+
+}
---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //Напишите тут ваши методы
+
+    public static void print () {
+
+    }
+    public static void print (int m) {
+
+    }
+
+    public static void print (int m, int n) {
+
+    }
+
+    public static void print (String s) {
+
+    }
+
+    public static void print ( int m, String s) {
+
+    }
+
+
+
+
+}
